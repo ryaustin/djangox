0454a4a8a28bc165c6ec65f69f022900f8e27ea3
ry_austin <rrarra@gmail.com>
2023-05-10 10:27:14 -0400

    wired up djangox template

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Reservation
+from django.contrib.auth.decorators import login_required
 
 
 class HomePageView(TemplateView):
@@ -7,3 +10,12 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
+
+
+@login_required
+def reservation_list(request):
+    """A view that returns a list of reservations"""
+    reservation_list = Reservation.objects.all()
+    template_name = "pages/reservation_list.html"
+    data = {"reservation_list": reservation_list, "company": "Thunder Canyon"}
+    return render(request, template_name=template_name, context=data)
