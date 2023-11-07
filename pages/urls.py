07690afbd3905b7338f32d909663274e6f06a043
ry_austin <rrarra@gmail.com>
2023-11-06 22:06:00 -0500

    my cool djangoX site

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, protected_view
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("protected/", protected_view, name="protected"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 
 class HomePageView(TemplateView):
@@ -7,3 +9,8 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
+
+@login_required
+def protected_view(request):
+    template_name = "pages/about.html"
+    return render(request, template_name=template_name, context={'secret_message': "This message is only visible to logged-in users"})
